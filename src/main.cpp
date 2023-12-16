ff20baee36f6e7dd1c8ef1b7a8d9910797936dae
Guillermo Valverde <guillermovalverde42@gmail.com>
2023-12-16 13:15:51 -0600

    para clase 16 dec

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,92 @@
 #include <Arduino.h>
-#include <AsyncTCP.h>
-#include <ESPAsyncWebServer.h>
 #include <WiFi.h>
+#include <HTTPClient.h>
+#include <Arduino_JSON.h>
+#include <FS.h>
 
-const char* ssid = "memovalverd_ESP32";
+const char* ssid = "memovalverd_2.4G";
 const char* password = "seguridadOUT#23";
 
-WiFiServer server(1001);
 
-void setup( void ){
+// THE DEFAULT TIMER IS SET TO 10 SECONDS FOR TESTING PURPOSES
+// For a final application, check the API call limits per hour/minute to avoid getting blocked/banned
+unsigned long lastTime = 0;
+// Timer set to 10 minutes (600000)
+//unsigned long timerDelay = 600000;
+// Set timer to 10 seconds (10000)
+unsigned long timerDelay = 10000;
+
+String jsonBuffer;
+
+String httpGETRequest(const char* serverName);
+
+void setup() {
   Serial.begin(9600);
 
-  delay(3000);
-
-  WiFi.softAP(ssid, password);
-  Serial.println(WiFi.softAPIP());
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting");
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
+ 
+  Serial.println("Timer set to 10 seconds (timerDelay variable), it will take 10 seconds before publishing the first reading.");
 }
 
-void loop( void ) {
-  WiFiClient client = server.available();
-
-  if( client ){
-    Serial.println("Nuevo cliente!");
-    while( client.connected() ){
-      String data = client.readString();
-      Serial.print("Dato recibido: ");
-      Serial.println(data);
-
-      client.print("Hola desde ESP32");
+void loop() {
+  // Send an HTTP GET request
+  if ((millis() - lastTime) > timerDelay) {
+    // Check WiFi connection status
+    if(WiFi.status()== WL_CONNECTED){
+      String serverPath = "https://rickandmortyapi.com/api/character/2";
+      
+      jsonBuffer = httpGETRequest(serverPath.c_str());
+      Serial.println(jsonBuffer);
+      JSONVar myObject = JSON.parse(jsonBuffer);
+  
+      // JSON.typeof(jsonVar) can be used to get the type of the var
+      if (JSON.typeof(myObject) == "undefined") {
+        Serial.println("Parsing input failed!");
+        return;
+      }
+    
+        Serial.print("JSON object = ");
+        Serial.println(myObject);
     }
-    client.stop();
-    Serial.println("Cliente desconectado");
+    else {
+      Serial.println("WiFi Disconnected");
+    }
+    lastTime = millis();
   }
+}
+
+String httpGETRequest(const char* serverName) {
+  String payload = "{}"; 
+  WiFiClientSecure client;
+//   WiFiClient client;
+  HTTPClient http;
+
+  // Your Domain name with URL path or IP address with path
+  http.begin(client, serverName);
+  
+  // Send HTTP POST request
+  int httpResponseCode = http.GET();
+  
+  if (httpResponseCode>0) {
+    Serial.print("HTTP Response code: ");
+    Serial.println(httpResponseCode);
+    payload = http.getString();
+  }
+  else {
+    Serial.print("Error code: ");
+    Serial.println(httpResponseCode);
+  }
+
+  // Free resources
+  http.end();
+
+  return payload;
 }
