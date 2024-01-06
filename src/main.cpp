f64f49fce1e42f69a59425fe27233a45b3d63da3
Guillermo Valverde <guillermovalverde42@gmail.com>
2024-01-06 10:23:11 -0600

    clase dic

--- src/main.cpp
+++ src/main.cpp
@@ -1,92 +1,92 @@
 #include <Arduino.h>
 #include <WiFi.h>
-#include <HTTPClient.h>
-#include <Arduino_JSON.h>
-#include <FS.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
 
-const char* ssid = "memovalverd_2.4G";
-const char* password = "seguridadOUT#23";
+AsyncWebServer server(81);
 
+const char html[] PROGMEM = R"rawliteral(
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <title>Control LEDS</title>
+</head>
+<body class="m-5">
+    <h1>LED CONTROL</h1>
+    <div class="form-check form-switch">
+        <input class="form-check-input" type="checkbox" role="switch" id="2" onchange="toggleCheckbox(this)">
+        <label class="form-check-label">LED 2</label>
+    </div>
 
-// THE DEFAULT TIMER IS SET TO 10 SECONDS FOR TESTING PURPOSES
-// For a final application, check the API call limits per hour/minute to avoid getting blocked/banned
-unsigned long lastTime = 0;
-// Timer set to 10 minutes (600000)
-//unsigned long timerDelay = 600000;
-// Set timer to 10 seconds (10000)
-unsigned long timerDelay = 10000;
+    <script>
+        const toggleCheckbox = async(element) => {
+            const url = `/update?state=${element.checked ? 1 : 0}`;
+          
+            const resp = await fetch(url, {
+                method: 'GET',
+            });
+            if(!resp.ok){
+                console.log("Error");
+            }else{
+                console.log(await resp.text());
+            }
+        }
+    </script>
 
-String jsonBuffer;
+</body>
+</html>
+)rawliteral";
 
-String httpGETRequest(const char* serverName);
+const char* ssid = "SALA 201";
+const char* password = "CG.PAGES";
+
+const char* PARAM_MESSAGE = "state";
+
+void notFound(AsyncWebServerRequest *request) {
+    request->send(404, "text/plain", "Not found");
+}
 
 void setup() {
-  Serial.begin(9600);
 
-  WiFi.begin(ssid, password);
-  Serial.println("Connecting");
-  while(WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.print("Connected to WiFi network with IP Address: ");
-  Serial.println(WiFi.localIP());
- 
-  Serial.println("Timer set to 10 seconds (timerDelay variable), it will take 10 seconds before publishing the first reading.");
+    Serial.begin(9600);
+
+    pinMode(2, OUTPUT);
+
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
+    if (WiFi.waitForConnectResult() != WL_CONNECTED) {
+        Serial.printf("WiFi Failed!\n");
+        return;
+    }
+
+    delay(3000);
+    Serial.print("IP Address: ");
+    Serial.println(WiFi.localIP());
+
+    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+      request->send(200, "text/html", html);
+    });
+
+    // Send a GET request to <IP>/update?state=<message>
+    server.on("/update", HTTP_GET, [] (AsyncWebServerRequest *request) {
+        String state;
+        if (request->hasParam(PARAM_MESSAGE)) {
+            state = request->getParam(PARAM_MESSAGE)->value();
+
+            digitalWrite(2, state.toInt());
+        } else {
+            state = "-1";
+        }
+        request->send(200, "text/plain", state);
+    });
+
+    server.onNotFound(notFound);
+
+    server.begin();
 }
 
 void loop() {
-  // Send an HTTP GET request
-  if ((millis() - lastTime) > timerDelay) {
-    // Check WiFi connection status
-    if(WiFi.status()== WL_CONNECTED){
-      String serverPath = "https://rickandmortyapi.com/api/character/2";
-      
-      jsonBuffer = httpGETRequest(serverPath.c_str());
-      Serial.println(jsonBuffer);
-      JSONVar myObject = JSON.parse(jsonBuffer);
-  
-      // JSON.typeof(jsonVar) can be used to get the type of the var
-      if (JSON.typeof(myObject) == "undefined") {
-        Serial.println("Parsing input failed!");
-        return;
-      }
-    
-        Serial.print("JSON object = ");
-        Serial.println(myObject);
-    }
-    else {
-      Serial.println("WiFi Disconnected");
-    }
-    lastTime = millis();
-  }
-}
-
-String httpGETRequest(const char* serverName) {
-  String payload = "{}"; 
-  WiFiClientSecure client;
-//   WiFiClient client;
-  HTTPClient http;
-
-  // Your Domain name with URL path or IP address with path
-  http.begin(client, serverName);
-  
-  // Send HTTP POST request
-  int httpResponseCode = http.GET();
-  
-  if (httpResponseCode>0) {
-    Serial.print("HTTP Response code: ");
-    Serial.println(httpResponseCode);
-    payload = http.getString();
-  }
-  else {
-    Serial.print("Error code: ");
-    Serial.println(httpResponseCode);
-  }
-
-  // Free resources
-  http.end();
-
-  return payload;
 }
